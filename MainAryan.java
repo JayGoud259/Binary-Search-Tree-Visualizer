260de18ae41253d2d770056a188f80bc611e1054
Aryanvishwakaarma <legendgameryt40@gmail.com>
2026-04-20 14:08:53 +0530

    Create MainAryan.java

---
+++ MainAryan.java
@@ -0,0 +1,74 @@
+//THIS CODE HAS BEEN WRITTEN BY ARYAN VISHWAKARMA 1012411175
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class Main {
+    static Node root;
+
+    static Node insert(Node root, int key) {
+        if (root == null) return new Node(key);
+        if (key < root.data) root.left = insert(root.left, key);
+        else if (key > root.data) root.right = insert(root.right, key);
+        return root;
+    }
+
+    static Node delete(Node root, int key) {
+        if (root == null) return root;
+
+        if (key < root.data) root.left = delete(root.left, key);
+        else if (key > root.data) root.right = delete(root.right, key);
+        else {
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+
+            Node temp = minValueNode(root.right);
+            root.data = temp.data;
+            root.right = delete(root.right, temp.data);
+        }
+        return root;
+    }
+
+    static Node minValueNode(Node node) {
+        Node current = node;
+        while (current.left != null)
+            current = current.left;
+        return current;
+    }
+
+    static int height(Node root) {
+        if (root == null) return -1;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    static void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+            root = insert(root, sc.nextInt());
+
+        int key = sc.nextInt();
+        root = delete(root, key);
+
+        inorder(root);
+        System.out.println();
+        System.out.println(height(root));
+    }
+}
